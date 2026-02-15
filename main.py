9296a275453c0184a0f2cc6ec1d5fcb713a106a0
AdamTimpson <adamtimpson@gmail.com>
2026-02-15 20:49:30 +0000

    Getting ready to draw tiles based on a map

--- main.py
+++ main.py
@@ -1,72 +1,77 @@
 from __future__ import annotations
 import tkinter as tk
 
-WINDOW_TITLE = "PyCraft"
-WINDOW_WIDTH = 900
-WINDOW_HEIGHT = 600
+WINDOW_TITLE: str = "PyCraft"
+WINDOW_WIDTH: int = 1920
+WINDOW_HEIGHT: int = 1080
 
-FPS = 60
-FRAME_TIME = int(1000 / FPS)
+FPS: int= 60
+FRAME_TIME: int = int(1000 / FPS)
 
-root: tk.Tk = None
-canvas: tk.Canvas = None
+X_TILE_COUNT: int = 48
+Y_TILE_COUNT: int = 27
 
-class Entity:
-    name: str = None
-    xpos: int = None 
-    ypos: int = None 
-    width: int = None 
-    height: int = None 
-    xpos_end: int = None 
-    ypos_end: int = None
-    color: str = None
+tiles: list[Tile] = []
 
-    def __init__(self, xpos: int, ypos: int, width: int, height: int):
-        self.xpos = xpos 
-        self.ypos = ypos
-        self.width = width
-        self.height = height
-        self.xpos_end = self.xpos + self.width
-        self.ypos_end = self.ypos + self.height
+root: tk.Tk = tk.Tk()
+canvas: tk.Canvas = tk.Canvas(root, width=WINDOW_WIDTH, height=WINDOW_HEIGHT, bg="black")
 
-    def draw(self, canvas: tk.Canvas):
-        print("Drawing... " + str(self))
-        canvas.create_rectangle(self.xpos, self.ypos, self.xpos_end, self.ypos_end, fill=f"{self.color}")
+class Tile: 
+    id: int = 0
+    xpos: int = 0
+    ypos: int = 0
+    width: int = int(WINDOW_WIDTH / X_TILE_COUNT)
+    height: int = int(WINDOW_HEIGHT / Y_TILE_COUNT)
+    fill_colour: str = "white"
 
-    def __str__(self):
-        return f"{self.name} ({self.xpos}, {self.ypos})"
+    def __init__(self, xpos: int, ypos: int, fill_colour="white") -> None:
+        self.id = Tile.id + 1 # Static?? 
+        Tile.id = Tile.id + 1 # I don't like this but I don't like largest_tile_id either...
+        self.xpos = xpos
+        self.ypos = ypos 
+        self.fill_colour = fill_colour
 
+    def __str__(self) -> str:
+        return f"{self.id}: ({self.xpos}, {self.ypos})"
 
-class Player(Entity):
-    def __init__(self, xpos: int, ypos: int):
-        self.name = "Player"
-        self.xpos = xpos 
-        self.ypos = ypos
-        self.width = 20
-        self.height = 20
-        self.color = "red"
-        super().__init__(xpos, ypos, 20, 20)
-
-
-def update():
-    print("Running...")
-
+def update() -> None:
     root.after(FPS, update)
 
-def main():
-    global root
-    root = tk.Tk()
+def init_window() -> None: 
     root.title(WINDOW_TITLE)
     root.geometry(f"{WINDOW_WIDTH}x{WINDOW_HEIGHT}")
 
-    global canvas
-    canvas = tk.Canvas(root, width=WINDOW_WIDTH, height=WINDOW_HEIGHT, bg="black")
-    player = Player(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2)
-    player.draw(canvas)
-
     canvas.pack()
-
+    
     update()
+
+def draw_grid() -> None:
+    for i in range(0, X_TILE_COUNT):
+        for j in range(0, Y_TILE_COUNT):
+            tile = Tile(i * Tile.width, j * Tile.height)
+            print(tile)
+            
+            tiles.append(tile)
+            fill: str = "black"
+    
+            if tile.id % 10 == 0 and tile.id % 2 == 0:
+                fill="green"
+
+            canvas.create_rectangle(
+                    tile.xpos, 
+                    tile.ypos, 
+                    tile.xpos + tile.width, 
+                    tile.ypos + tile.height, 
+                    fill=fill)
+            
+def init_game() -> None:
+    draw_grid()
+
+def main() -> None:
+    init_window()
+
+    init_game()
+
     root.mainloop()
 
 if __name__ == "__main__": 
